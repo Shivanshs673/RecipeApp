4c66794d56c75de46d0096d2c609b565d669a505
Shivanshs673 <shivanshs673@gmail.com>
2024-12-13 22:46:39 +0530

    update

--- app/src/main/java/com/example/recipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/RecipeScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -82,7 +81,6 @@ fun CategoryItem(
                 .fillMaxSize()
                 .aspectRatio(1f)
         )
-
         Text(
             text = category.strCategory,
             color = Color.Black,
